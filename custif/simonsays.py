30f66596bf3c08d0afa95b7cf4573c87182c5f33
Chris Lee <chrislee015@gmail.com>
2021-08-04 17:42:23 +0000

    reason for commit using bash

---
+++ custif/simonsays.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import time
+
+print("Welcome to Simon Says Game")
+count = 1;
+current_simon_command = "x"
+time.sleep(2.5)
+print("Thank you for playing!")
+
+
+
+def printSimon(count):
+    print(current_simon_command)
+    print(" " * 1000)
+
+printSimon(1111)
